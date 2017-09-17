501f23cef530d6828180fefe5a3bff163530fdbb
madrinathapa <madrinathapa.rs@gmail.com>
2017-09-16 20:34:41 -0400

    Dockerized hello world(node) service

--- Assignment1/Services/hello-world/main.js
+++ Assignment1/Services/hello-world/main.js
@@ -1,16 +1,17 @@
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(err, conn) {
+amqp.connect('amqp://rmq-container', function(err, conn) {
   conn.createChannel(function(err, ch) {
     var ex = 'amq.direct';
+    var queue ='greet';
 
     ch.assertExchange(ex, 'direct', {durable: true});
 
     ch.assertQueue('', {exclusive: true}, function(err, q) {
       console.log(' [*] Waiting for logs. To exit press CTRL+C');
 
-    ch.bindQueue(q.queue, ex, "greet");
-    ch.consume(q.queue, function(msg) {
+    ch.bindQueue(queue, ex, "greet");
+    ch.consume(queue, function(msg) {
         var response = JSON.stringify({ result: "Welcome "+ msg.content.toString() +"!" });
         ch.publish(ex, "greetresponse", new Buffer(response));
         console.log(" [x] Sent %s: '%s'", "greetresponse", response);
