e6a66f08198c66fa1618a566cdfdc9a618c2c035
madrinathapa <madrinathapa.rs@gmail.com>
2017-09-17 20:46:12 -0400

    Updated the host name by VM's public IP for greet service

--- Assignment1/Services/Circle-Service/circleRabbitMQ.py
+++ Assignment1/Services/Circle-Service/circleRabbitMQ.py
@@ -2,58 +2,30 @@ import pika
 import sys
 import json
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='rmq-container'))
+print("[x] starts here")
+
+credentials = pika.PlainCredentials('test', 'test')
+parameters = pika.ConnectionParameters('129.114.104.76',credentials=credentials)
+
+connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
+channel.queue_declare(queue='circle', durable=True)
 
-channel.exchange_declare(exchange='amq.direct',
-                         exchange_type='direct', durable=True)
+def getArea(radius):
+    area = 3.14*float(radius)*float(radius)
+    result = {'radius':float(radius),'area': float(area)}
+    return json.dumps(result)
 
-result = channel.queue_declare(exclusive=True)
-queue_name = result.method.queue
-
-print("queue name %r" % (queue_name))
-
-binding_key = "circle"
-channel.queue_bind(exchange="amq.direct",
-                  queue=queue_name,
-                  routing_key=binding_key)
-
-
-print(' [*] Waiting for logs. To exit press CTRL+C')
-
-class Circle:
-    def __init__(self):
-        self.cache = {}
-    def __call__(self, radius):
-        area = 3.14*float(radius)*float(radius)
-        result = {'radius':float(radius),'area': float(area)}
-        return json.dumps(result)
-
-cir = Circle()
-
-class Publish:
-    def __init__(self):
-        self.cache={}
-    def __call__(self, msg):
-        channel.basic_publish(exchange='amq.direct',
-                              routing_key="circleresponse",
-                              body=msg)
-        print(" [x] Sent %r:%r" % ("circle response", msg))
-
-pub = Publish()
+def publish(msg):
+    channel.basic_publish(exchange='amq.direct', routing_key="circleresponse", body=msg)
+    print(" [x] Sent %r:%r" % ("circle response", msg))
 
 def callback(ch, method, properties, body):
-    result = cir(body)
+    print("Body: %r" % (body))
+    result = getArea(body)
     print("Final result: %r" % (result))
-    pub(result)
+    publish(result)
     print(" [x] %r:%r" % (method.routing_key, body))
 
-result = channel.queue_declare(exclusive=True)
-queue_name = result.method.queue
-
-channel.basic_consume(callback,
-                      queue=queue_name,
-                      no_ack=True)
-
+channel.basic_consume(callback, queue='circle', no_ack=True)
 channel.start_consuming()
